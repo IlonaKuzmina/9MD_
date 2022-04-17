a41fd7ad29c2705efbdf0ba185695e38f0ae9c83
IlonaKuzmina <ikuzmina88@gmail.com>
2022-04-18 00:58:21 +0300

    html DONE

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,33 @@
-console.log('Ready for coding');
+// console.log('Ready for coding');
+// const toast = document.querySelector<HTMLDivElement>('.toast');
+// const showToast = document.querySelector<HTMLDivElement>('.show-toast');
+
+// showToast.addEventListener('click', () => {
+//    toast.classList.add('toast-active')})
+
+///  INPUT ////   BUTTON
+
+// const inputText = document.querySelector<HTMLInputElement>('.js-input-text');
+
+// close toster ///
+const moreViews = document.querySelector<HTMLDivElement>('.toast');
+const moreBtns = document.querySelector<HTMLButtonElement>('.hero__button--watch');
+const moreClose = document.querySelector('.toast__close');
+
+// const more = function (moreClick) {
+//   moreViews[moreClick].classList.add('active-more');
+// };
+
+// moreBtns.forEach((mb, i) => {
+//   mb.addEventListener('click', () => {
+//     more(i);
+//   });
+// });
+
+// moreClose.forEach((mc) => {
+//   mc.addEventListener('click', () => {
+//     moreViews.forEach((mv) => {
+//       mv.classList.remove('active-more');
+//     });
+//   });
+// });
